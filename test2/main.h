2d6faa801c114480dc54f375cf981d1a7dbecb8d
tykoon787 <tykoon787@protonmail.com>
2022-10-21 01:11:23 +0300

    Added Structure

--- test2/main.h
+++ test2/main.h
@@ -1,2 +1,21 @@
-/* Functions Prototype */
-/* Structure Prototype */
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+
+/**
+ * struct special_characters- Structure containing Special Characters
+ * and their respective function calls
+ * @s: The special character itself e.g., 'c' || 's'
+ * @f: Function Pointer
+ */
+
+typedef struct special_characters
+{
+	char *s;
+	int (*f)(va_list);
+} spec;/* Specifier */
+
+#endif /* MAIN_H */
+
+
