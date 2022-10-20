eee29c21795395190a01a6c164f0332f633537ce
BrianEtemesi <etemecci@gmail.com>
2022-10-20 22:39:00 +0000

    updated prototypes

--- test2/main.h
+++ test2/main.h
@@ -1,2 +1,12 @@
-/* Functions Prototype */
-/* Structure Prototype */
+#ifndef MAIN_H
+#define MAIN_H
+
+int _printf(const char *format, ...);
+char *_strcpy(char *dest, char *src);
+int _putchar(char c);
+int _strlen(char *str);
+int print_char(va_list arg_c);
+int print_str(va_list arg_str);
+int (*check_format(const char *format))(va_list);
+
+#endif /* MAIN_H */
