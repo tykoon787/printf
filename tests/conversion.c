6b75a2c54a9ec64c8b250b7aa069164e7fa8b204
tykoon787 <tykoon787@protonmail.com>
2022-10-20 11:25:59 +0300

    Tweaked Code

--- tests/conversion.c
+++ tests/conversion.c
@@ -6,10 +6,11 @@
 
 int _printf(const char *format, ...)
 {
-	int i = 0, returnValue = 22, size = 0, len = 0;
+	int i = 0, count = 0, returnValue = 22, size = 0, len = 0;
 	va_list args; 
 	char x;
-	char *buffer, *str_arg; 
+	char *buffer, *str_arg, *cpy_buffer; 
+	
 
 	va_start(args, format);
 
@@ -19,78 +20,63 @@ int _printf(const char *format, ...)
 		len++;
 		size++;
 	}
-	printf("Length of format: %d\n", len);
 	size = 0;
 	
 	/* Allocate Space to buffer */
-	buffer = malloc(sizeof(char) * len);
+	buffer = malloc(sizeof(char) * 1000);
 	if (buffer == NULL)
+	{
 		return (-1);
+	}
+	else
+	{
+		cpy_buffer = buffer;
+	}
 
 	for (; i < len; i++)
 	{
-		printf("Loop number %d\n",i);
 		if (format[i] == '%')
 		{
 			switch(format[i + 1])
 			{
 				case 'c':
 					buffer[i] = (char) va_arg(args, int);
-					printf("%c\n", x);
-					printf("Character\n");
 					i++;
+					count++;
 					break;
 				case 's':
 					str_arg = va_arg(args, char *);
 					strcpy(&buffer[i], str_arg);
-					printf("This is a string\n");
+					printf("this is a string\n");
 					i++;
+					count++;
 					break;
 				case 'd':
 					buffer[i] = va_arg(args, int);
-					// Buffer stores int as a char,
-					// Conversion needed during output
-					printf("This is a decimal value\n");
+					 /* Buffer stores int as a char, */
+					 /* Conversion needed during output */
 					i++;
+					count++;
 					break;
 			}
 		}
 		else
 		{
 			buffer[i] = format[i];
+			count++;
 		}
 	}
-	/* Loop through format to check for format spec */
-	/* for (; i < len; i++) */
-	/* { */
-	/* printf("Loop number: [%d]\n", i); */
-	/* 	 Copy the contents of format to buffer  */
-	/* 	if ((format[i] != '%') || (format[i+1] != 'c')) */
-	/* 	{ */
-	/* 		printf("(if) Copying buffer \n"); */
-	/* 		buffer[i] = format[i]; */
-	/* 	} */
-	/*  */
-	/* 	switch(format[i]) */
-	/* 	{ */
-	/* 		case '%': */
-	/* 		if (format[i + 1] == 'c') */
-	/* 		buffer[i] = (char) va_arg(args, int); */
-	/* 		printf("%c\n", x); */
-	/* 	} */
-	/* } */
-	printf("Copied buffer %d times\n",i);
+	
+	
 
 	/* Write the buffer to stdout */
-	printf("Size of buffer is :%d\n", (int)sizeof(buffer));
-	write(1, buffer, len+1);
-	putchar('\n');
+	returnValue = (int) write(1, buffer, count + strlen(str_arg) + 1);
 
 	return (returnValue);
 }
 
 int main(void)
 {
-	_printf("%c Hello %c W %sorld %d", 'x',"Baby Panda", 'b', 33);
+	_printf("Hello %c World %c and %s", 'x', 'b', "Panda");
 	return (0);
 }
