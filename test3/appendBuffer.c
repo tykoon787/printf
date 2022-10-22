6019ed9510c987ba3f42fbac26d39e8793cefcd1
tykoon787 <tykoon787@protonmail.com>
2022-10-22 15:48:39 +0300

    Modified Code to work with any buffer

--- test3/appendBuffer.c
+++ test3/appendBuffer.c
@@ -14,38 +14,54 @@
 char *rtn_buffer()
 {
 
-	char *buffer = malloc(sizeof(char) * 1024);
+	char *buffer = malloc(BUFFSIZE);
 	snprintf(buffer, BUFFSIZE, "%s", "Baby Panda ");
 	return (buffer);
 }
 
-char *append_buffer(char *str)
+char *append_buffer(char *buff_add, char *str)
 {
-	char *buff_add = rtn_buffer();
-
+	/* Check to See what the buffer contains. If it has anything, copy 
+	* all their contenst to the main buffer */
+	char *apd_buff = rtn_buffer();
 	int len = 0;
-	while (buff_add[len] != '\0')
+
+	while (apd_buff[len] != '\0')
 	{
+		printf("Searching through buffer\n");
 		len++;
 
 	}
 	printf("Reached End of Buffer. Start Appending\n");
 
+	/* First append main buffer with buff_add */
 	int i = 0;
-	while (str[i] != '\0')
+	while (buff_add[i] != '\0')
 	{
-		buff_add[len] = str[i];
+		printf("Appending Main Buffer with buffer_add\n");
+		apd_buff[len] = buff_add[i];
 		len++;
 		i++;
 	}
-	return(buff_add);
+
+	/* Then append buffer with str */
+	i = 0;
+	while (str[i] != '\0')
+	{
+		apd_buff[len] = str[i];
+		len++;
+		i++;
+	}
+	buff_add = apd_buff;
+	return(apd_buff);
 }
 
 
 int main(void)
 {
 	char str1[] = "Loves Babmoo";
-	char *buffer = append_buffer(str1);
+	int *main_buffer = malloc(BUFFSIZE);
+	char *buffer = append_buffer((char *)main_buffer, str1);
 	printf("Heap Address Allocated :%p\n", buffer);
 
 	/* Printing Appended Buffer */
@@ -55,4 +71,8 @@ int main(void)
 		putchar(buffer[i]);
 		i++;
 	}
+
+	putchar('\n');
+	/* printf("Animals Like %s love the letter %c and the word %% %s %%\n",  */
+	/* "Pandas", 'x', "Bamboo"); */
 }
